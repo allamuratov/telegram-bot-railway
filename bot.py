3c79947367e70b64a42dba677908a6093f72e52f
Aydos Allamuratov <mr.allamuratov@gmail.com>
2025-05-24 17:05:39 +0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,29 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
+
+# /start komandasi uchun funksiya
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    group_link = "https://t.me/nokis_tashken_almati"
+    keyboard = [[InlineKeyboardButton("👥 Guruhga qo‘shilish", url=group_link)]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text(
+        "Assalomu alaykum!\n\nQuyidagi tugma orqali guruhimizga qo‘shiling: \n"
+        "https://t.me/nokis_tashken_almati\n"
+        "https://t.me/tashkent_nokis1",
+        reply_markup=reply_markup
+    )
+
+# Botni ishga tushirish
+async def main():
+    token = os.environ.get("BOT_TOKEN")
+    app = ApplicationBuilder().token(token).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+    await app.run_polling()
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
